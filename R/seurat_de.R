23ec3153c692bb5da8b7f73cf096ae97b02b9b77
Tim <tbarry2@andrew.cmu.edu>
2022-04-14 22:52:36 -0400

    handle case of 0 genes DE in seurat_de

--- R/seurat_de.R
+++ R/seurat_de.R
@@ -23,54 +23,45 @@
 #' result <- seurat_de(response_odm, gRNA_odm, response_gRNA_group_pairs)
 #' }
 seurat_de <- function(response_odm, gRNA_odm, response_gRNA_group_pairs) {
-  # Step 0: load response ODM and gRNA ODM into memory
   response_mat <- load_whole_odm(response_odm)
   gRNA_mat <- load_whole_odm(gRNA_odm)
-  gRNA_feature_covariates <- gRNA_odm |> ondisc::get_feature_covariates()
-
-  # Step 1: assign perturbation identities to cells using the procedure outline in the Methods section of Papalexi 2021
-  # i. compute gRNA library sizes
+  gRNA_feature_covariates <- ondisc::get_feature_covariates(gRNA_odm)
   cell_lib_sizes <- Matrix::colSums(gRNA_mat)
-  # ii. remove cells with a gRNA library size of less than 5
   ok_cells <- cell_lib_sizes >= 5
+  cell_covariates <- ondisc::get_cell_covariates(response_odm)
   if (!all(ok_cells)) {
-    response_mat <- response_mat[,ok_cells]
-    gRNA_mat <- gRNA_mat[,ok_cells]
+    response_mat <- response_mat[, ok_cells]
+    gRNA_mat <- gRNA_mat[, ok_cells]
+    cell_covariates <- cell_covariates[ok_cells, ]
   }
-  # iii. find the gRNA with the highest count in each cell, and assign it to that cell (confirmed match to Papalexi)
   gRNA_assignments <- apply(X = gRNA_mat, MARGIN = 2, FUN = function(col) names(which.max(col)))
-  cell_metadata <- data.frame(perturbation = gRNA_assignments) |>
-    dplyr::mutate(ondisc::get_cell_covariates(response_odm) |> dplyr::select(-n_nonzero, -n_umis))
-
-  # Step 2: Put the gene expressions into a Seurat object
-  seurat_obj <- Seurat::CreateSeuratObject(counts = response_mat, assay = "RNA", meta.data = cell_metadata)
+  cell_metadata <- dplyr::mutate(data.frame(perturbation = gRNA_assignments),
+                                 dplyr::select(cell_covariates, -n_nonzero, -n_umis))
+  seurat_obj <- Seurat::CreateSeuratObject(counts = response_mat,
+                                           assay = "RNA", meta.data = cell_metadata)
   rm(response_mat)
-
-  # Step 3: Normalize the expression data (consider also scTransform)
   seurat_obj <- Seurat::NormalizeData(seurat_obj)
-
-  # Step 4: Aggregate the negative controls (and ONLY negative controls)
-  neg_control_gRNAs <- gRNA_feature_covariates |>
-    dplyr::filter(target_type == "non-targeting") |>
-    row.names()
-  aggregated_gRNAs <- ifelse(cell_metadata$perturbation %in% neg_control_gRNAs, "NT", cell_metadata$perturbation)
+  neg_control_gRNAs <- row.names(dplyr::filter(gRNA_feature_covariates,
+                                               target_type == "non-targeting"))
+  aggregated_gRNAs <- ifelse(cell_metadata$perturbation %in%
+                               neg_control_gRNAs, "NT", cell_metadata$perturbation)
   seurat_obj$aggregate_gRNA <- aggregated_gRNAs
   Seurat::Idents(seurat_obj) <- "aggregate_gRNA"
-
-  # Step 5: Carry out the differential expression analysis gRNA group by gRNA group
   unique_gRNA_groups <- as.character(unique(response_gRNA_group_pairs$gRNA_group))
   res_list <- lapply(X = unique_gRNA_groups, FUN = function(curr_gRNA) {
-    curr_response_gRNA_group_pairs <- response_gRNA_group_pairs |>
-      dplyr::filter(gRNA_group == curr_gRNA)
-    # run FindMarkers
-    markers_res <- Seurat::FindMarkers(object = seurat_obj, ident.1 = curr_gRNA,
-                               ident.2 = "NT", only.pos = FALSE,
-                               logfc.threshold = 0.25, test.use = "wilcox")
-    data.frame(response_id = row.names(markers_res),
-               gRNA_group = curr_gRNA,
-               p_value = markers_res$p_val)
+    curr_response_gRNA_group_pairs <- dplyr::filter(response_gRNA_group_pairs, gRNA_group == curr_gRNA)
+    markers_res <- Seurat::FindMarkers(object = seurat_obj,
+                                       ident.1 = curr_gRNA, ident.2 = "NT", only.pos = FALSE,
+                                       logfc.threshold = 0.25, test.use = "wilcox")
+    if (nrow(markers_res) == 0) {
+      ret <- NULL
+    } else {
+      ret <- data.frame(response_id = row.names(markers_res), gRNA_group = curr_gRNA,
+                        p_value = markers_res$p_val)
+    }
+    return(ret)
   })
-  res <- dplyr::bind_rows(res_list)
+  res <- dplyr::bind_rows(l = res_list)
   return(res)
 }
 
