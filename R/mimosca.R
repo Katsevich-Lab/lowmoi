0b0ebe8b500bc31e73245d5d0efca688fb56fa13
Tim <tbarry2@andrew.cmu.edu>
2022-05-27 01:00:00 -0400

    finish mimosca method

--- R/mimosca.R
+++ R/mimosca.R
@@ -5,42 +5,48 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' response_odm <- load_dataset_modality("frangieh/control/gene")
-#' response_odm <- response_odm[1:100,]
-#' gRNA_odm <- load_dataset_modality("frangieh/control/grna")
-#' response_gRNA_group_pairs <- data.frame(response_id = response_odm |> ondisc::get_feature_ids(),
-#' gRNA_group = "TTGCGGCCTCGATACGATAT")
+#' response_odm <- load_dataset_modality("schraivogel/ground_truth_tapseq/gene")
+#' gRNA_odm <- load_dataset_modality("schraivogel/ground_truth_tapseq/grna")
+#' response_gRNA_group_pairs <- expand.grid(response_id = (response_odm |> ondisc::get_feature_ids()),
+#' gRNA_group = c("GATA1-C", "GATA1-D"))
+#' result <- mimosca(response_odm, gRNA_odm, response_gRNA_group_pairs)
 #' }
-mimosca <- function(response_odm, gRNA_odm, response_gRNA_group_pairs) {
-  if (FALSE) {
-  # first, load the data
-  response_mat <- load_whole_odm(response_odm)
-  gRNA_mat <- load_whole_odm(gRNA_odm)
-  gRNAs <- unique(response_gRNA_group_pairs$gRNA_group)
+mimosca <- function(response_odm, gRNA_odm, response_gRNA_group_pairs, n_rep = 50) {
+  # load the data, transposing the matrices (as is required by mimosca)
+  response_mat_t <- Matrix::t(load_whole_odm(response_odm))
+  response_names <- colnames(response_mat_t)
+  gRNA_mat_t <- Matrix::t(load_whole_odm(gRNA_odm))
+  # assign gRNA IDs to cells via the max operation
+  gRNA_mat_t_bin <- apply(X = gRNA_mat_t, MARGIN = 1, FUN = function(col) {
+    out <- integer(length = ncol(gRNA_mat_t))
+    out[which.max(col)] <- 1L
+    return(out)
+  }) |> Matrix::t()
+  gRNA_mat_t_bin <- as(gRNA_mat_t_bin, "dgCMatrix") == 1
+  gRNA_names <- colnames(gRNA_mat_t)
 
-  use_python("/Library/Frameworks/Python.framework/Versions/3.10/bin/python3")
-  # simple example: increment an integer
-  setwd("/Users/timbarry/research_code/lowmoi/")
-  ret <- import("mimosca")
+  # load the python functions
+  retic <- tryCatch({
+    reticulate::py_run_file(system.file("python", "mimosca.py", package = "lowmoi"))
+  }, error = function(cond) {
+    reticulate::py_run_file(system.file("python", "mimosca.py", package = "lowmoi"))
+  })
 
-  # define get_pieces function
-  get_pieces <- function(csc_mat) {
-    list(csc_mat@x, csc_mat@i, csc_mat@p, csc_mat@Dim[1], csc_mat@Dim[2])
-  }
-
-  # load example data
-  response_odm <- lowmoi::load_dataset_modality("frangieh/control/gene")
-  response_odm <- response_odm[1:100,]
-  gRNA_odm <- lowmoi::load_dataset_modality("frangieh/control/grna")
-  my_gRNA <- "TTGCGGCCTCGATACGATAT"
-  response_gRNA_group_pairs <- data.frame(response_id = response_odm |> get_feature_ids(),
-                                          gRNA_group = my_gRNA)
-  response_mat_t <- Matrix::t(lowmoi::load_whole_odm(response_odm))
-  gRNA_mat_t <- Matrix::t(lowmoi::load_whole_odm(gRNA_odm))
-  # call mimosca
-  cov_ind <- which(my_gRNA == colnames(gRNA_mat_t)) - 1L
-  out <- ret$run_mimosca(get_pieces(response_mat_t), get_pieces(gRNA_mat_t), cov_ind, 30)
-  }
+  # apply mimosca
+  unique_gRNAs <- as.character(unique(response_gRNA_group_pairs$gRNA_group))
+  res_list <- lapply(unique_gRNAs, function(curr_gRNA) {
+      cov_ind <- which(curr_gRNA == gRNA_names) - 1L
+      p_vals <- retic$run_mimosca(get_sparse_matrix_pieces(response_mat_t),
+                        get_sparse_matrix_pieces(gRNA_mat_t),
+                        cov_ind,
+                        n_rep)
+      out_df <- data.frame(response_id = response_names,
+                           gRNA_group = curr_gRNA,
+                           p_val = p_vals)
+  })
+  ret <- dplyr::left_join(response_gRNA_group_pairs,
+                          do.call(what = "rbind", args = res_list))
+  return(ret)
 }
 
 
