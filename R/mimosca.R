c08819f28123b632413031b102edfa4354e2ea28
Tim <tbarry2@andrew.cmu.edu>
2022-05-28 21:49:51 -0400

    change default n_rep in mimosca to 1000

--- R/mimosca.R
+++ R/mimosca.R
@@ -7,11 +7,10 @@
 #' \dontrun{
 #' response_odm <- load_dataset_modality("schraivogel/ground_truth_tapseq/gene")
 #' gRNA_odm <- load_dataset_modality("schraivogel/ground_truth_tapseq/grna")
-#' response_gRNA_group_pairs <- expand.grid(response_id = (response_odm |> ondisc::get_feature_ids()),
-#' gRNA_group = c("GATA1-C", "GATA1-D"))
+#' response_gRNA_group_pairs <- expand.grid(response_id = (response_odm |> ondisc::get_feature_ids()), gRNA_group = c("GATA1-C", "GATA1-D"))
 #' result <- mimosca(response_odm, gRNA_odm, response_gRNA_group_pairs)
 #' }
-mimosca <- function(response_odm, gRNA_odm, response_gRNA_group_pairs, n_rep = 50) {
+mimosca <- function(response_odm, gRNA_odm, response_gRNA_group_pairs, n_rep = 1000) {
   # load the data, transposing the matrices (as is required by mimosca)
   response_mat_t <- Matrix::t(load_whole_odm(response_odm))
   response_names <- colnames(response_mat_t)
