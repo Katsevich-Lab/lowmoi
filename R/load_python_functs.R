e380176f372a40bc07bb2dee6b7e8949c024f1fb
ekatsevi <katsevich.gene@gmail.com>
2022-05-30 13:57:24 -0400

    Made python functions internal to the package

--- R/load_python_functs.R
+++ R/load_python_functs.R
@@ -5,24 +5,23 @@
   mimosca_module <- reticulate::import_from_path(module = "mimosca",
                                                  path = py_modules_path,
                                                  delay_load = TRUE)
-  run_mimosca <<- mimosca_module$run_mimosca
-  get_dense_array <<- mimosca_module$get_dense_array
+  assign('run_mimosca', mimosca_module$run_mimosca, envir = topenv())
 
   # Weissman (cell population class)
   cell_pop_module <- reticulate::import_from_path(module = "cell_population",
                                                   path = py_modules_path,
                                                   delay_load = TRUE)
-  CellPopulation <<- cell_pop_module$CellPopulation
+  assign('CellPopulation', cell_pop_module$CellPopulation, envir = topenv())
 
   # Weissman (expression normalization)
   exp_norm_module <- reticulate::import_from_path(module = "expression_normalization",
                                                   path = py_modules_path,
                                                   delay_load = TRUE)
-  normalize_to_gemgroup_control <<- exp_norm_module$normalize_to_gemgroup_control
+  assign('normalize_to_gemgroup_control', exp_norm_module$normalize_to_gemgroup_control, envir = topenv())
 
   # Weissman (differential expression)
   diff_exp_module <- reticulate::import_from_path(module = "differential_expression",
                                                   path = py_modules_path,
                                                   delay_load = TRUE)
-  ks_de <<- diff_exp_module$ks_de
+  assign('ks_de', diff_exp_module$ks_de, envir = topenv())
 }
