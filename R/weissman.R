60d034091f978b25bad7565f507e502ab0b89d9b
ekatsevi <katsevich.gene@gmail.com>
2022-06-01 13:07:55 -0400

    Finished implementing Weissman method

--- R/weissman.R
+++ R/weissman.R
@@ -8,7 +8,7 @@
 #' response_odm <- load_dataset_modality("schraivogel/ground_truth_tapseq/gene")
 #' gRNA_odm <- load_dataset_modality("schraivogel/ground_truth_tapseq/grna")
 #' response_gRNA_group_pairs <- expand.grid(response_id = (response_odm |> ondisc::get_feature_ids()), gRNA_group = c("GATA1-C", "GATA1-D"))
-#' result <- mimosca(response_odm, gRNA_odm, response_gRNA_group_pairs)
+#' result <- weissman(response_odm, gRNA_odm, response_gRNA_group_pairs)
 #' }
 weissman <- function(response_odm, gRNA_odm, response_gRNA_group_pairs) {
 
@@ -16,26 +16,50 @@ weissman <- function(response_odm, gRNA_odm, response_gRNA_group_pairs) {
   cell_pop <- odm_to_cell_pop(response_odm, gRNA_odm)
 
   # normalize data
-  cell_pop <- normalize_to_gemgroup_control(pop = cell_pop,
-                                            control_cells = 'guide_target == "non-targeting"')
+  cell_pop$normalized_matrix <- normalize_to_gemgroup_control(
+    pop = cell_pop,
+    control_cells = 'guide_target == "non-targeting"'
+  )
 
-  # apply mimosca
+  # apply Weissman method
   unique_gRNAs <- as.character(unique(response_gRNA_group_pairs$gRNA_group))
   res_list <- lapply(unique_gRNAs, function(curr_gRNA) {
-    cov_ind <- which(curr_gRNA == gRNA_names) - 1L
-    p_vals <- run_mimosca(get_sparse_matrix_pieces(response_mat_t),
-                          get_sparse_matrix_pieces(gRNA_mat_t),
-                          cov_ind,
-                          n_rep)
-    out_df <- data.frame(response_id = response_names,
-                         gRNA_group = curr_gRNA,
-                         p_value = p_vals)
-  })
-  ret <- dplyr::left_join(response_gRNA_group_pairs,
-                          do.call(what = "rbind", args = res_list))
-  return(ret)
-}
+    # find responses to test this gRNA against
+    response_vars <- response_gRNA_group_pairs |>
+      dplyr::filter(gRNA_group == curr_gRNA) |>
+      dplyr::pull(response_id) |>
+      as.character()
 
+    # get normalized matrix for cells with this gRNA and genes to test against
+    cell_pop_targeting <- cell_pop$where(
+      cells = sprintf('guide_identity == \"%s\"', curr_gRNA),
+      genes = response_vars |> add_ENSG(),
+      normalized = TRUE
+    )
+
+    # get normalized matrix for cells with control gRNAs and genes to test against
+    cell_pop_control <- cell_pop$where(
+      cells = 'guide_target == "non-targeting"',
+      genes = response_vars |> add_ENSG(),
+      normalized = TRUE
+    )
+
+    # apply the python function ks_compare_pops
+    p_vals <- ks_compare_pops(cell_pop_targeting, cell_pop_control)[[2]]
+    stopifnot(all(names(p_vals) == response_vars |> add_ENSG()))
+    out_df <- data.frame(
+      response_id = response_vars,
+      gRNA_group = curr_gRNA,
+      p_value = unname(p_vals)
+    )
+    out_df
+  })
+  # concatenate and return results
+  dplyr::left_join(
+    response_gRNA_group_pairs,
+    do.call(what = "rbind", args = res_list)
+  )
+}
 
 #' Converse from ODM to CellPopulation format
 #'
@@ -43,43 +67,73 @@ weissman <- function(response_odm, gRNA_odm, response_gRNA_group_pairs) {
 #' @param gRNA_odm  ODM for gRNAs
 #'
 #' @return An object of type `CellPopulation` that the `perturbseq` library expects
-odm_to_cell_pop <- function(response_odm, gRNA_odm){
+odm_to_cell_pop <- function(response_odm, gRNA_odm) {
   # load the data, transposing the matrices
-  # TODO: Make proper indexed pandas data frame out of this
   response_mat_t <- response_odm |>
     load_whole_odm() |>
     Matrix::t() |>
+    as.matrix() |>
     as.data.frame()
 
+  # add ENSG to column names for compatibility with perturbseq software
+  colnames(response_mat_t) <- colnames(response_mat_t) |> add_ENSG()
   feature_ids <- ondisc::get_feature_ids(response_odm)
   feature_names <- ondisc::get_feature_names(response_odm)
-  genes_df <- tibble::tibble(gene_id = feature_ids)
-  if(!all(is.na(feature_names))){
-    genes_df$gene_name <- feature_names
+  # if feature names are not present, let feature ids be feature names
+  if (all(is.na(feature_names))) {
+    feature_names <- feature_ids
   }
+  feature_ids <- feature_ids |> add_ENSG()
 
+  # create genes_df for input to CellPopulation constructor
+  genes_df <- data.frame(gene_name = feature_names, row.names = feature_ids)
+
+  # assign gRNAs to cells based on max rule
+  # (temporary...eventually should make gRNA assignment part of dataset)
   gRNA_mat <- load_whole_odm(gRNA_odm)
   gRNA_names <- rownames(gRNA_mat)
   guide_identities <- gRNA_names[apply(X = gRNA_mat, MARGIN = 2, which.max)]
 
+  # create cells_df for input to CellPopulation constructor
   cells_df <- ondisc::get_cell_covariates(response_odm) |>
     tibble::rownames_to_column(var = "cell_barcode") |>
-    dplyr::mutate(guide_identity = guide_identities)
-  if(!("batch" %in% names(cells_df))){
-    cells_df$batch <- 1
+    dplyr::mutate(guide_identity = guide_identities) # add gRNA assignments
+  if (!("batch" %in% names(cells_df))) {
+    cells_df$batch <- 1 # add batch information if it is not present
   }
   cells_df <- cells_df |>
-    dplyr::left_join(gRNA_odm |>
-                       ondisc::get_feature_covariates() |>
-                       tibble::rownames_to_column(var = "guide_identity") |>
-                       dplyr::select(guide_identity, target),
-                     by = "guide_identity") |>
+    dplyr::left_join(gRNA_odm |> # join with gRNA metadata to get guide targets
+      ondisc::get_feature_covariates() |>
+      tibble::rownames_to_column(var = "guide_identity") |>
+      dplyr::select(guide_identity, target),
+    by = "guide_identity"
+    ) |>
     dplyr::rename(UMI_count = n_umis, gem_group = batch, guide_target = target) |>
     dplyr::mutate(gem_group = as.integer(as.factor(gem_group))) |>
-    dplyr::select(cell_barcode, UMI_count, gem_group, guide_identity, guide_target)
+    dplyr::select(cell_barcode, UMI_count, gem_group, guide_identity, guide_target) |>
+    tibble::column_to_rownames(var = "cell_barcode")
 
-  CellPopulation(matrix = response_mat_t,
-                 cell_list = cells_df,
-                 gene_list = genes_df,
-                 calculate_statistics = FALSE)
+  # construct and return the CellPopulation object expected by perturbseq software
+  CellPopulation(
+    matrix = response_mat_t,
+    cell_list = cells_df,
+    gene_list = genes_df
+  )
+}
+
+#' Add ENSG prefix to a list of feature ids if necessary
+#'
+#' This is somewhat of a hack needed for compatibility with perturbseq code.
+#'
+#' @param feature_id Vector of feature ids
+#'
+#' @return The same set of feature ids with ENSG prefixes added if necessary
+add_ENSG <- function(feature_ids) {
+  sapply(feature_ids, function(feature_id) {
+    if (substr(feature_id, start = 0, stop = 4) != "ENSG") {
+      paste0("ENSG_", feature_id)
+    } else {
+      feature_id
+    }
+  }) |> unname()
 }
