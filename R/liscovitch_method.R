a16377613ce7ed479a50ae90b4cc99632f748e17
Tim <tbarry2@andrew.cmu.edu>
2022-06-30 12:31:00 -0400

    update liscovitch method -- use less memory

--- R/liscovitch_method.R
+++ R/liscovitch_method.R
@@ -13,15 +13,14 @@
 #' res <- liscovitch_method(response_odm, gRNA_odm, response_gRNA_group_pairs)
 #' }
 liscovitch_method <- function(response_odm, gRNA_odm, response_gRNA_group_pairs) {
-  chrom_data <- as.matrix(load_whole_odm(response_odm))
+  exp_data <- load_whole_odm(response_odm)
   # obtain the library sizes
   cell_covariates <- response_odm |> ondisc::get_cell_covariates()
   lib_size_col_name <- if ("n_fragments" %in% colnames(cell_covariates)) "n_fragments" else "n_umis"
   cell_lib_sizes <- cell_covariates[[lib_size_col_name]]
   # divide by the number of fragments per cell
-  chrom_norm <- t(t(chrom_data)/cell_lib_sizes)
-  # convert into (row-wise) z-scores
-  z_score_mat <- apply(X = chrom_norm, MARGIN = 1, FUN = function(curr_row) scale(curr_row)[,1]) |> t()
+  exp_data_norm <- Matrix::t(Matrix::t(exp_data)/cell_lib_sizes)
+  rm(exp_data)
 
   # assign gRNAs to cells; obtain the indexes of the negative control grnas
   gRNA_targets <- get_target_assignments_via_max_op(gRNA_odm)
@@ -31,8 +30,11 @@ liscovitch_method <- function(response_odm, gRNA_odm, response_gRNA_group_pairs)
   p_vals <- apply(X = response_gRNA_group_pairs, MARGIN = 1, FUN = function(r) {
     grna <- as.character(r[["gRNA_group"]])
     response <- as.character(r[["response_id"]])
-    target_cells <- z_score_mat[response, grna == gRNA_targets]
-    control_cells <- z_score_mat[response, neg_control_idxs]
+    response_row <- exp_data_norm[response,]
+    response_row_scale <- scale(response_row)[,1]
+
+    target_cells <- response_row_scale[grna == gRNA_targets]
+    control_cells <- response_row_scale[neg_control_idxs]
     fit <- stats::t.test(target_cells, control_cells)
     fit$p.value
   })
