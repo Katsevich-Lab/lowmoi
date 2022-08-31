1357341249c195acff274de65aa0d3a86b937652
Tim <tbarry2@andrew.cmu.edu>
2022-08-30 20:09:51 -0400

    fix output of sceptre funct

--- R/sceptre_functs.R
+++ R/sceptre_functs.R
@@ -3,11 +3,15 @@
 #' Implements sceptre2.
 #'
 #' @inherit abstract_interface
+#' @param B number of resamples to draw
+#' @param reduced output (TRUE) or intermed
 #' @export
-sceptre <- function(response_odm, grna_odm, response_grna_group_pairs) {
+sceptre <- function(response_odm, grna_odm, response_grna_group_pairs, B = 100000, output_amount = 1) {
+  if (!is.numeric(B)) B <- as.integer(B)
+  if (!is.numeric(output_amount)) output_amount <- as.integer(output_amount)
+
   # construct the multimodal ODM
-  mm_odm <- ondisc::multimodal_ondisc_matrix(list(response = response_odm,
-                                                  grna = grna_odm)) |>
+  mm_odm <- ondisc::multimodal_ondisc_matrix(list(response = response_odm, grna = grna_odm)) |>
     lowmoi::process_multimodal_odm()
 
   # get the formula
@@ -20,13 +24,14 @@ sceptre <- function(response_odm, grna_odm, response_grna_group_pairs) {
                                        response_modality_name = "response",
                                        grna_modality_name = "grna",
                                        grna_group_column_name = "target",
-                                       B = 2500,
+                                       B = B,
                                        side = "both",
-                                       full_output = FALSE)
+                                       output_amount = output_amount) |> as.data.frame()
 
   # select p_val, grna_grop, response_id
-  res_select <- res |>
-    dplyr::select(response_id, grna_group, p_value) |>
-    as.data.frame()
-  return(res_select)
+  if (output_amount == 1) {
+    res <- res |> dplyr::select(response_id, grna_group, p_value)
+  }
+
+  return(res)
 }
