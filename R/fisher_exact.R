5f0e40d25415ca788628f3bfe17b9562ac0388ef
Tim <tbarry2@andrew.cmu.edu>
2022-10-10 19:38:00 -0400

    change default threshold in fisher test from zero to median

--- R/fisher_exact.R
+++ R/fisher_exact.R
@@ -3,16 +3,22 @@
 #' Carries out Fisher's exact test.
 #'
 #' @param progress print progress statements?
+#' @param threshold threshold to use to categorize cells into "highly expressed" and "lowly expressed" groups; one of "median" and "zero" (for now)
 #' @inherit abstract_interface
 #' @export
-fisher_exact <- function(response_odm, grna_odm, response_grna_group_pairs, progress = TRUE) {
+fisher_exact <- function(response_odm, grna_odm, response_grna_group_pairs, progress = TRUE, threshold = "median") {
   # define the Fisher exact two sample test
   two_sample_test <- function(target_cells, control_cells, target_cell_indices, control_cell_indices) {
+    if (threshold == "median") {
+      thresh <- median(c(target_cells, control_cells))
+    } else if (threshold == "zero") {
+      thresh <- 0
+    }
     # create the contingency table
-    n_treatment_zero <- sum(target_cells == 0)
-    n_treatment_exp <- sum(target_cells >= 1)
-    n_control_zero <- sum(control_cells == 0)
-    n_control_exp <- sum(control_cells >= 1)
+    n_treatment_zero <- sum(target_cells <= thresh)
+    n_treatment_exp <- sum(target_cells > thresh)
+    n_control_zero <- sum(control_cells <= thresh)
+    n_control_exp <- sum(control_cells > thresh)
 
     contingency_table <- matrix(data = c(n_treatment_zero, n_treatment_exp,
                                          n_control_zero, n_control_exp),
