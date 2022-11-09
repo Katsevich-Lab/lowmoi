c1e5895d8055677f9d8f6008ea336e3d51048cbc
Tim <tbarry2@andrew.cmu.edu>
2022-11-09 18:40:36 -0500

    add covariate option to sceptre (lowmoi)

--- R/sceptre_functs.R
+++ R/sceptre_functs.R
@@ -5,18 +5,25 @@
 #' @inherit abstract_interface
 #' @param B number of resamples to draw
 #' @param output_amount amount of output to return from function (ranging from 1-3)
+#' @param sn_approx use a skew-normal approximation?
+#' @param with_covariates should covariates (beyond log-transformed library size) be included in the model?
 #' @export
-sceptre <- function(response_odm, grna_odm, response_grna_group_pairs, B = 2500, output_amount = 1, sn_approx = TRUE) {
+sceptre <- function(response_odm, grna_odm, response_grna_group_pairs, B = 2500, output_amount = 1, sn_approx = TRUE, with_covariates = TRUE) {
   if (!is.numeric(B)) B <- as.integer(B)
   if (!is.numeric(output_amount)) output_amount <- as.integer(output_amount)
   if (!is.logical(sn_approx)) sn_approx <- as.logical(sn_approx)
+
 
   # construct the multimodal ODM
   mm_odm <- ondisc::multimodal_ondisc_matrix(list(response = response_odm, grna = grna_odm)) |>
     lowmoi::process_multimodal_odm()
 
   # get the formula
-  form <- mm_odm@modalities$response@misc$sceptre_formula
+  if (with_covariates) {
+    form <- mm_odm@modalities$response@misc$sceptre_formula
+  } else {
+    form <- stats::formula(~log(response_n_umis))
+  }
 
   # call function
   res <- sceptre2::run_sceptre_low_moi(mm_odm = mm_odm,
