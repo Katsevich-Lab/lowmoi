d7e3ad428ea6cb9ac4014b7104ce0b0d30c3d51d
Tim <tbarry2@andrew.cmu.edu>
2023-04-18 13:05:22 -0400

    remove no-longer-needed sceptre functs

--- R/sceptre_functs.R
+++ R/sceptre_functs.R
@@ -3,7 +3,7 @@
 #' @inherit abstract_interface
 #'
 #' @export
-sceptre <- function(response_odm, grna_odm, response_grna_group_pairs, with_covariates = TRUE, regression_method = "poisson_glm", discovery_test_stat = "approximate", print_progress = TRUE) {
+sceptre <- function(response_odm, grna_odm, response_grna_group_pairs, with_covariates = TRUE, print_progress = TRUE) {
   # load the gene and grna matrix into memory
   response_matrix <- load_whole_odm(response_odm)
   grna_matrix <- load_whole_odm(grna_odm)
@@ -15,13 +15,11 @@ sceptre <- function(response_odm, grna_odm, response_grna_group_pairs, with_cova
     lowmoi::process_multimodal_odm()
 
   covariate_data_frame <- mm_odm |> ondisc::get_cell_covariates()
-  formula_object <- response_odm@misc$sceptre_formula
-
   # get the formula
   if (with_covariates) {
-    form <- mm_odm@modalities$response@misc$sceptre_formula
+    formula_object <- mm_odm@modalities$response@misc$sceptre_formula
   } else {
-    form <- stats::formula(~log(response_n_umis))
+    formula_object <- stats::formula(~log(response_n_umis))
   }
 
   # run sceptre
@@ -32,64 +30,12 @@ sceptre <- function(response_odm, grna_odm, response_grna_group_pairs, with_cova
                               formula_object = formula_object,
                               response_grna_group_pairs = response_grna_group_pairs,
                               calibration_check = FALSE,
-                              print_progress = print_progress,
-                              regression_method = regression_method,
-                              discovery_test_stat = discovery_test_stat) |>
+                              print_progress = print_progress) |>
     dplyr::select(response_id, grna_group, p_value)
   return(res)
 }
 
 
 sceptre_no_covariates <- function(response_odm, grna_odm, response_grna_group_pairs) {
-  sceptre(response_odm, grna_odm, response_grna_group_pairs)
-}
-
-
-#' @export
-#' @inherit abstract_interface
-sceptre_approximate_poisson <- function(response_odm, grna_odm, response_grna_group_pairs) {
-  sceptre(response_odm = response_odm,
-          grna_odm = grna_odm,
-          response_grna_group_pairs = response_grna_group_pairs,
-          with_covariates = TRUE,
-          regression_method = "poisson_glm",
-          discovery_test_stat = "approximate",
-          print_progress = TRUE)
-}
-
-
-#' @export
-#' @inherit abstract_interface
-sceptre_exact_poisson <- function(response_odm, grna_odm, response_grna_group_pairs) {
-  sceptre(response_odm = response_odm,
-          grna_odm = grna_odm,
-          response_grna_group_pairs = response_grna_group_pairs,
-          with_covariates = TRUE,
-          regression_method = "poisson_glm",
-          discovery_test_stat = "exact",
-          print_progress = TRUE)
-}
-
-#' @export
-#' @inherit abstract_interface
-sceptre_approximate_nb <- function(response_odm, grna_odm, response_grna_group_pairs) {
-  sceptre(response_odm = response_odm,
-          grna_odm = grna_odm,
-          response_grna_group_pairs = response_grna_group_pairs,
-          with_covariates = TRUE,
-          regression_method = "nb_glm",
-          discovery_test_stat = "approximate",
-          print_progress = TRUE)
-}
-
-#' @export
-#' @inherit abstract_interface
-sceptre_exact_nb <- function(response_odm, grna_odm, response_grna_group_pairs) {
-  sceptre(response_odm = response_odm,
-          grna_odm = grna_odm,
-          response_grna_group_pairs = response_grna_group_pairs,
-          with_covariates = TRUE,
-          regression_method = "nb_glm",
-          discovery_test_stat = "exact",
-          print_progress = TRUE)
+  sceptre(response_odm, grna_odm, response_grna_group_pairs, with_covariates = FALSE)
 }
